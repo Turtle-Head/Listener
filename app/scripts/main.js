9d1de5b3a2652e74728c906678d9a0b0180c2cfc
James Fehr <fehr.james@gmail.com>
2016-03-04 15:50:37 -0600

    Part 1 of API integration

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -21,24 +21,53 @@ $(function(){
         client_id: CLIENT_ID
       });
 
-      var track_url = track || 'http://soundcloud.com/forss/flickermood';
-      SC.oEmbed(track_url, { auto_play: true }).then(function(oEmbed) {
-        console.log('oEmbed response: ', oEmbed);
-      });
+      if (track !== '') {
+        var track_url = track;
+        SC.oEmbed(track_url, { auto_play: true }).then(function(oEmbed) {
+          console.log('oEmbed response: ', oEmbed);
+          $('.current_track').html(oEmbed.html);
+        });
+      }
     };
+
+    sc_emb();
+
+    var results = [];
 
     var sc_search = function() {
       SC.initialize({
         client_id: CLIENT_ID
       });
 
+      var search = $('#new_search').val();
+
+      // checks for a value in the input field, if there search for the value
+      if(search !== ''){
+        SC.get('/tracks', {
+          q: search
+        }).then(function(tracks) {
+          console.log(tracks);
+          results = [];
+          $('#results').html('');
+          for (var index = 0; index < tracks.length; index++){
+            results.push(tracks[index]);
+            $('#results').append('<div class="res_li" id=' + index + '>' + tracks[index].title + '<div><hr>' );
+          }
+        });
+      }
+
       // find all tracks with the genre 'punk' that have a tempo greater than 120 bpm.
-      SC.get('/tracks', {
+      // Removed
+      /*SC.get('/tracks', {
         genres: 'punk', bpm: { from: 120 }
       }).then(function(tracks) {
         console.log(tracks);
-      });
+      });*/
     };
+
+    $('.res_li').on("click", function(){
+      console.log(results[Number(this.id)].permalink_url);
+    });
 
     $(".nav_item").on("click", function(){
       switch(this.id){
